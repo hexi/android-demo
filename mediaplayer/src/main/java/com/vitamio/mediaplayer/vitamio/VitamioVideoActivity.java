958a511b9050b79670b7824cbc3876582472b0ac
hexi <xi.he@baidao.com>
2016-03-25 10:15:42 +0800

    视频

--- mediaplayer/src/main/java/com/vitamio/mediaplayer/vitamio/VitamioVideoActivity.java
+++ mediaplayer/src/main/java/com/vitamio/mediaplayer/vitamio/VitamioVideoActivity.java
@@ -3,7 +3,8 @@ package com.vitamio.mediaplayer.vitamio;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.FrameLayout;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.vitamio.mediaplayer.R;
 
@@ -15,27 +16,57 @@ import io.vov.vitamio.widget.VideoView;
  */
 public class VitamioVideoActivity extends Activity {
     private static final String TAG = "VitamioAudioActivity";
-    String audioPath = "rtmp://live.hkstv.hk.lxdns.com/live/hks";
+    String path = "rtmp://live1.evideocloud.net/live/test1__8Z2MPDMkP4Nm";
     VideoView videoView;
+    ProgressBar progress;
+    private boolean needResume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_view);
-        videoView = new VideoView(this);
-//        videoView = (VideoView) findViewById(R.id.surface_view);
+        videoView = (VideoView) findViewById(R.id.surface_view);
+        progress = (ProgressBar) findViewById(R.id.progress);
 
-        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.fl_video_container);
-        frameLayout.addView(videoView);
+//        videoView = new VideoView(this);
+//        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.fl_video_container);
+//        frameLayout.addView(videoView);
 
-        videoView.setVideoPath(audioPath);
+        videoView.setVideoPath(path);
         videoView.requestFocus();
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
 
             @Override
             public void onPrepared(MediaPlayer mp) {
                 Log.d(TAG, "===onPrepared===");
-                videoView.start();
+                startVideo();
+            }
+        });
+
+        videoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+            @Override
+            public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                switch (what) {
+                    case MediaPlayer.MEDIA_INFO_BUFFERING_START:
+                        //Begin buffer, pauseVideo playing
+                        if (videoView.isPlaying()) {
+                            needResume = true;
+                            pauseVideo();
+                        }
+                        showProgressBar();
+                        break;
+                    case MediaPlayer.MEDIA_INFO_BUFFERING_END:
+                        //The buffering is done, resume playing
+                        hideProgressBar();
+                        if (needResume)
+                            startVideo();
+                        break;
+                    case MediaPlayer.MEDIA_INFO_DOWNLOAD_RATE_CHANGED:
+                        //Display video download speed
+                        Log.d(TAG, "download rate:" + extra);
+                        break;
+                }
+                return true;
             }
         });
 
@@ -53,6 +84,22 @@ public class VitamioVideoActivity extends Activity {
                 return true;
             }
         });
+    }
+
+    private void startVideo() {
+        videoView.start();
+    }
+
+    private void hideProgressBar() {
+        this.progress.setVisibility(View.GONE);
+    }
+
+    private void showProgressBar() {
+        this.progress.setVisibility(View.VISIBLE);
+    }
+
+    private void pauseVideo() {
+        videoView.pause();
     }
 
     @Override

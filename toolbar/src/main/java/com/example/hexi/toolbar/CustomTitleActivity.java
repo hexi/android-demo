e963903a036f72d0d34ef993ad72113e6698c7cf
hexi <xi.he@baidao.com>
2017-07-18 17:57:27 +0800

    [toolbar]状态栏变色

--- toolbar/src/main/java/com/example/hexi/toolbar/CustomTitleActivity.java
+++ toolbar/src/main/java/com/example/hexi/toolbar/CustomTitleActivity.java
@@ -3,20 +3,25 @@ package com.example.hexi.toolbar;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.ViewTreeObserver;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.example.hexi.toolbar.widgets.YtxTitle;
 
 public class CustomTitleActivity extends FragmentActivity {
+    private static final String TAG = "CustomTitleActivity";
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_title);
-
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
         int color = Color.parseColor("#de1111");
         YtxTitle ytxTitle = (YtxTitle) findViewById(R.id.ytx_title);
-        ytxTitle.setBackgroundColor(color);
+        ytxTitle.setBackgroundColor(Color.TRANSPARENT);
         ytxTitle.setOnActionListener(new YtxTitle.OnActionClickListener() {
             @Override
             public void onClickedLeftAction() {
@@ -30,7 +35,24 @@ public class CustomTitleActivity extends FragmentActivity {
             }
         });
 
-        StatusBarUtil.setStatusBarColor(this, color);
+        StatusBarUtil.hideStatusBar(this);
+        final int targetY = getResources().getDimensionPixelSize(R.dimen.common_title_height);
+        Log.d(TAG, "===targetY:" + targetY);
+        scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            int preScrollY = 0;
+
+            @Override
+            public void onScrollChanged() {
+                int scrollY = scrollView.getScrollY();
+                Log.d(TAG, String.format("===onScrollChanged, preScrollY:%d, scrollY:%d", preScrollY, scrollY));
+                if (scrollY >= targetY) {
+                    StatusBarUtil.setStatusBarColor(CustomTitleActivity.this, Color.WHITE);
+                } else if (scrollY < targetY) {
+                    StatusBarUtil.hideStatusBar(CustomTitleActivity.this);
+                }
+                preScrollY = scrollY;
+            }
+        });
     }
 
 
--- toolbar/src/main/java/com/example/hexi/toolbar/StatusBarUtil.java
+++ toolbar/src/main/java/com/example/hexi/toolbar/StatusBarUtil.java
@@ -1,6 +1,7 @@
 package com.example.hexi.toolbar;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Build;
 import android.view.Window;
 import android.view.WindowManager;
@@ -11,6 +12,14 @@ import android.view.WindowManager;
 
 public class StatusBarUtil {
 
+    public static void hideStatusBar(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
     public static void setStatusBarColor(Activity activity, int color) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = activity.getWindow();
@@ -18,5 +27,10 @@ public class StatusBarUtil {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(color);
         }
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        return context.getResources().getDimensionPixelSize(resourceId);
     }
 }
